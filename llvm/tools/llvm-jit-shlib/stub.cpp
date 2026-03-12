6ea198432ef02a164025777c59ca946a2e858c32
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2026-03-12 10:13:21 +0100

    Introduce libKNHR (Kotlin/Native Hot-Reload Library)
    Create a new custom LLVM component used to support HotReload runtime
    within Kotlin/Native's one. The library uses the new LLVM ORCv2 APIs,
    with JITLink support, to create new symbols at runtime and overposing
    theme (through stubs creation). The library is created by setting the
    CMake variable `-DLLVM_BUILD_KOTLIN_JIT_DYLIB=ON`.

---
+++ llvm/tools/llvm-jit-shlib/stub.cpp
@@ -0,0 +1,12 @@
+//===- stub.cpp - Kotlin/Native Hot Reload Shared Library -------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Empty file required by CMake. The actual symbols come from LLVM component
+// libraries linked into this shared library.
+//
+//===----------------------------------------------------------------------===//
